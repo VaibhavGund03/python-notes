274be9c14b6d5f3ce3b9180924a028b58bebec35
vaibhav gund <vaibhav.gcluster@gmail.com>
2024-12-08 19:02:00 +0530

    5-12-2024

---
+++ 01_Basics/coments.py
@@ -0,0 +1,6 @@
+# this is a single line comment use (#).
+""" this is a multile line comment
+  by sung this sign crate a multiple line comments """
+
+
+print("hello world")
--- 01_Basics/hello.py
+++ 01_Basics/hello.py
@@ -3,7 +3,6 @@ print("hello Vaibahv")
 ''' def = defination '''
 def vaibhav(n):
     print(n)
-
 vaibhav(4)
 '''this is a first code for practice'''
 
