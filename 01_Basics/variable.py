968b2e6809b4b2771d81073d882bf960f876f50e
vaibhav gund <vaibhav.gcluster@gmail.com>
2024-12-09 08:35:25 +0530

    9-12-2024

---
+++ 01_Basics/variable.py
@@ -0,0 +1,67 @@
+# variable a  continear that store a any value in program
+""" suppose you have give a number in a program that number are store in any variable
+     a = 20 .   in this concept a is a variable that hold inside a value  this value is 20 .are hold in variable a"""
+
+a = "vaibhav"
+print(a)
+
+b = 21+20
+print(b)
+
+
+v = 23
+p = 20
+
+print(v+p)
+#method 1 ......................................
+#write a program swap two variable
+
+x= 12
+y = 20
+
+temp = x
+print(temp)
+
+x = y
+print(x)
+y = temp
+print(y)
+print("value of x is ",x)  #output is 20
+print("value of y is ",y)  #output is 12
+
+#method 2.......................................
+
+p = 12
+q = 13
+
+p,q = q,p
+
+print(p) #output is 13
+print(q) #output is 12
+
+
+# write a program to convert float in to integer
+num = 12.3
+print(type(num))
+num = int(num)
+print(type(num))
+print(num)
+
+# write a program to take detail from a student for ID-card and then print it different lines
+print("student identity card")
+name = input("inter the name of the student : ")
+grade = input("enter the grade of the student : ")
+age = input("enter the age of the student")
+b_date = input("enter the baday date of a student: ")
+Email = input("enter the Email of the student :")
+phno = input("enter the phone number of the student : ")
+print()
+print("..........!! Student Details Sucessfully Added..............!!")
+print(name)
+print(grade)
+print(age)
+print(b_date)
+print(Email)
+print(phno)
+
+
---
+++ 01_Basics/variable2.py
@@ -0,0 +1,7 @@
+# write a program to take an user input as integer then convert into float
+a = int(input("enter the number : "))
+print(a)
+print(type(a))
+a = float(a)
+print(a)
+print(type(a))
